3ad9c8a3365861c57e91560c968dc145d8e50aa4
uabmiguel <1706041@uab.cat>
2025-11-29 13:48:18 +0100

    crank 1.1

--- crank-nicholson.py
+++ crank-nicholson.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# from codigoanalitica import analitica
+from codigoanalitica import analitica
 
 # Metóde Jacobi resoldre Ax = b
 def jacobi(A, b, x0, max_iter=50):
@@ -25,7 +25,7 @@ def jacobi(A, b, x0, max_iter=50):
 # Parametres
 L = 2
 N = 101
-Ta = 0.025    
+ta = 0.025    
 Pext = (0.472*40**2)/(2*L**2)
 To = 309.65*((0.56)/(Pext*L**2))  
 x = np.linspace(0, 2, N)
@@ -33,6 +33,8 @@ x = np.linspace(0, 2, N)
 deltax = 1 / (N - 1)
 deltat_list = [(deltax)**2, 0.5*((deltax)**2)]    
 
+T_list = []
+err_list = []
 for deltat in deltat_list:
     gamma = deltat / deltax**2
     # Condició inicial
@@ -57,7 +59,7 @@ for deltat in deltat_list:
 
     # Bucle
     t = 0.0
-    while t < Ta:
+    while t < ta:
         b = (B @ T) + deltat 
         T = jacobi(A, b, T)  
         # if any(((phi[0:36]/((0.56)/(Pext * L**2)))-273.15) > 50) == True:
@@ -65,8 +67,12 @@ for deltat in deltat_list:
         #     break
         t += deltat
 
-    plt.plot(x,(T/((0.56)/(Pext * L**2)))-273.15, label = f"$\\Delta T = {deltat/(deltax**2)} * (\\Delta X)²$")
+    T_list.append((T/((0.56)/(Pext * L**2)))-273.15)
+    err_list.append(abs(analitica(ta) - ((T/((0.56)/(Pext * L**2)))-273.15)))
 
+# Gràfica per cada cas
+plt.plot(x, T_list[0], label = f"$\\Delta T = \\Delta X)²$")
+plt.plot(x, T_list[1], label = f"$\\Delta T = 0.5 \\Delta X)²$")
 plt.hlines(50,0,2,color='r',linestyle='--')
 plt.hlines(80,0,2,color='r',linestyle='--')
 plt.vlines(0.75,36.5,80,color='r',linestyle='--')
@@ -75,5 +81,13 @@ plt.xlabel('Posició (cm)')
 plt.ylabel('Temperatura  (ºC)')
 plt.legend()
 plt.grid()
+plt.show()
 
+# Gràfica d'errors 
+plt.plot(x, err_list[0], label = f"$\\Delta T = \\Delta X)²$")
+plt.plot(x, err_list[1], label = f"$\\Delta T = 0.5 \\Delta X)²$")
+plt.xlabel('Posició (cm)')
+plt.ylabel('Error absolut (ºC)')
+plt.legend()
+plt.grid()
 plt.show()
