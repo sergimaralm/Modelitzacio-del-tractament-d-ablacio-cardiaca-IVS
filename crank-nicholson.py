418a7063ec43b923002fbdec08683372ee9639d1
uabmiguel <1706041@uab.cat>
2025-11-29 12:36:27 +0100

    crank 1.01

--- crank-nicholson.py
+++ crank-nicholson.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from codigoanalitica import analitica
+# from codigoanalitica import analitica
 
 # Metóde Jacobi resoldre Ax = b
 def jacobi(A, b, x0, max_iter=50):
@@ -23,55 +23,57 @@ def jacobi(A, b, x0, max_iter=50):
     return x
 
 # Parametres
-L = 2             
+L = 2
 N = 101
 Ta = 0.025    
-Pext=(0.472 * 40**2)/(2*L**2)
-To=309.65*((0.56)/(Pext * L**2))  
-
-dx = 1 / (N - 1)
-dt = 0.5* dx**2         
-
-r = dt / dx**2
+Pext = (0.472*40**2)/(2*L**2)
+To = 309.65*((0.56)/(Pext*L**2))  
 x = np.linspace(0, 2, N)
 
-# Condició inicial
-phi = To*np.ones(N)
+deltax = 1 / (N - 1)
+deltat_list = [(deltax)**2, 0.5*((deltax)**2)]    
 
-# Matrius A i B
-A = np.zeros((N, N))
-B = np.zeros((N, N))
+for deltat in deltat_list:
+    gamma = deltat / deltax**2
+    # Condició inicial
+    T = To*np.ones(N)
 
-for i in range(N):
-    # Diagonal principal
-    A[i, i] = 1 + r
-    B[i, i] = 1 - r
+    # Matrius A i B
+    A = np.zeros((N, N))
+    B = np.zeros((N, N))
 
-    # Sub y super diagonals
-    if i > 0:
-        A[i, i-1] = -r / 2
-        B[i, i-1] = r / 2
-    if i < N-1:
-        A[i, i+1] = -r / 2
-        B[i, i+1] = r / 2
+    for i in range(N):
+        # Diagonal principal
+        A[i, i] = 1 + gamma
+        B[i, i] = 1 - gamma
 
-# Bucle
-t = 0.0
-while t < Ta:
-    b = (B @ phi) + dt 
-    phi = jacobi(A, b, phi)  
-    # if any(((phi[0:36]/((0.56)/(Pext * L**2)))-273.15) > 50) == True:
-    #     print(t)
-    #     break
-    t += dt
+        # Sub y super diagonals
+        if i > 0:
+            A[i, i-1] = -gamma / 2
+            B[i, i-1] = gamma / 2
+        if i < N-1:
+            A[i, i+1] = -gamma / 2
+            B[i, i+1] = gamma / 2
 
-plt.plot(x,(phi/((0.56)/(Pext * L**2)))-273.15)
-plt.grid(True)
-plt.xlabel('Posició (cm)')
-plt.ylabel('Graus Centígrads')
+    # Bucle
+    t = 0.0
+    while t < Ta:
+        b = (B @ T) + deltat 
+        T = jacobi(A, b, T)  
+        # if any(((phi[0:36]/((0.56)/(Pext * L**2)))-273.15) > 50) == True:
+        #     print(t)
+        #     break
+        t += deltat
+
+    plt.plot(x,(T/((0.56)/(Pext * L**2)))-273.15, label = f"$\\Delta T = {deltat/(deltax**2)} * (\\Delta X)²$")
+
 plt.hlines(50,0,2,color='r',linestyle='--')
 plt.hlines(80,0,2,color='r',linestyle='--')
-plt.vlines(0.75,36.5,55,color='r',linestyle='--')
-plt.vlines(1.25,36.5,55,color='r',linestyle='--')
+plt.vlines(0.75,36.5,80,color='r',linestyle='--')
+plt.vlines(1.25,36.5,80,color='r',linestyle='--')
+plt.xlabel('Posició (cm)')
+plt.ylabel('Temperatura  (ºC)')
+plt.legend()
+plt.grid()
 
 plt.show()
